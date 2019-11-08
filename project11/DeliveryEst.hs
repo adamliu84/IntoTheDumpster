0c2a4dac27396d958a92af053f24459d7adc6ae2
ADAM LIU <adamliu84@fb.com>
2019-11-08 19:38:13 +0800

    Quick exception catch

--- project11/DeliveryEst.hs
+++ project11/DeliveryEst.hs
@@ -7,6 +7,7 @@ import Control.Lens ((&), (.~), (^.))
 import Data.Text (Text, pack)
 import Data.List.Split (splitOn)
 import Data.ByteString.Lazy.Char8 (unpack)
+import Control.Exception
 
 ad_account :: String
 ad_account = "act_<ID>"
@@ -31,6 +32,10 @@ genEstMau i ts = do
   let response = r ^. responseBody
   print $ concat [i,"->", getEstMau (unpack response)]
 
+tryEstMau :: String -> String -> IO ()
+tryEstMau i ts =
+  catch (genEstMau i ts) (\(SomeException _) -> print $ concat ["error on row:",i])
+
 getEstMau :: String -> String
 getEstMau input = takeWhile (\c -> c /= ',')  ((splitOn "estimate_mau\":" input)!!1)
 
@@ -38,4 +43,4 @@ main :: IO ()
 main = do
   contents <- readFile "temp.csv"
   let ts_rows = map parseRow (lines contents)
-  mapM_ (\x -> genEstMau (fst x) (snd x) ) ts_rows
+  mapM_ (\x -> tryEstMau (fst x) (snd x) ) ts_rows
