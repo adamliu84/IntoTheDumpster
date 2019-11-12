80715b777ad56295d9141ff234d7b7d74afdd67f
ADAM LIU <adamliu84@fb.com>
2019-11-12 15:53:53 +0800

    shift act_id and access_token to config file

--- project11/DeliveryEst.hs
+++ project11/DeliveryEst.hs
@@ -9,38 +9,39 @@ import Data.List.Split (splitOn)
 import Data.ByteString.Lazy.Char8 (unpack)
 import Control.Exception
 
-ad_account :: String
-ad_account = "act_<ID>"
+data Config = Config {ad_account_id :: String, access_token :: String} deriving (Show)
 
-access_token :: String
-access_token = "<ACCESS_TOKEN>"
-
-genEndPoint :: String
-genEndPoint = concat ["https://graph.facebook.com/v4.0/",ad_account,"/delivery_estimate"]
+genEndPoint :: String -> String
+genEndPoint ad_id = concat ["https://graph.facebook.com/v4.0/act_",ad_id,"/delivery_estimate"]
 
 parseRow :: String -> (String, String)
 parseRow row = (read y'::String, read (drop 1 z)::String)
     where (x,y) = break (\c -> c == ',') row
           (y',z) = break (\c -> c == ',') (drop 1 y)
 
-genEstMau :: String -> String -> IO ()
-genEstMau i ts = do
-  let opts = defaults & param "access_token" .~ [pack access_token]
+genEstMau :: Config -> String -> String -> IO ()
+genEstMau config i ts = do
+  let opts = defaults & param "access_token" .~ [pack $ access_token config]
              & param "targeting_spec" .~ [pack ts]
              & param "optimization_goal" .~ ["APP_INSTALLS"]
-  r <- (getWith opts genEndPoint)
+  r <- (getWith opts (genEndPoint (ad_account_id config)))
   let response = r ^. responseBody
   print $ concat [i,"->", getEstMau (unpack response)]
 
-tryEstMau :: String -> String -> IO ()
-tryEstMau i ts =
-  catch (genEstMau i ts) (\(SomeException _) -> print $ concat ["error on row:",i])
+tryEstMau :: Config -> String -> String -> IO ()
+tryEstMau config i ts =
+  catch (genEstMau config i ts) (\(SomeException _) -> print $ concat ["error on row:",i])
 
 getEstMau :: String -> String
 getEstMau input = takeWhile (\c -> c /= ',')  ((splitOn "estimate_mau\":" input)!!1)
 
+getConfig :: String -> Config
+getConfig c = Config (drop 14 (c'!!0)) (drop 13 (c'!!1))
+  where c' = lines c
+
 main :: IO ()
 main = do
+  config <- (return.getConfig) =<< readFile "config.ini"
   contents <- readFile "temp.csv"
   let ts_rows = map parseRow (lines contents)
-  mapM_ (\x -> tryEstMau (fst x) (snd x) ) ts_rows
+  mapM_ (\x -> tryEstMau config (fst x) (snd x) ) ts_rows
