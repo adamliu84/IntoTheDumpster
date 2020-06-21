86c5c35fba86ca4ce483ae63c9cc3d686ab62952
adamliu84 <adam.liu84@gmail.com>
2020-06-21 20:38:46 +0800

    Placeholder to call FB app evt

--- project14/flutter_test_app/android/app/src/main/kotlin/com/example/flutter_test_app/MainActivity.kt
+++ project14/flutter_test_app/android/app/src/main/kotlin/com/example/flutter_test_app/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.flutter_test_app
 
 import androidx.annotation.NonNull
+import com.facebook.FacebookActivity
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsConstants
+import com.facebook.appevents.AppEventsLogger
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import java.util.*
+
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        FacebookSdk.setIsDebugEnabled(true)
+        val logger = AppEventsLogger.newLogger(this)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor, "com.into.the.dumpster/flutter").setMethodCallHandler { call, result ->
             if (call.method == "hellofromflutter") {
+                logger.logEvent(AppEventsConstants.EVENT_NAME_ACHIEVED_LEVEL)
                 val productId = call.argument<Int>("productId")
                 result.success("Hello back, and you have selected product ID:" + productId)
             } else {
