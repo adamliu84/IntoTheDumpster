30757ed025ad818a8a8d6237f6652047285fb185
adamliu84 <adam.liu84@gmail.com>
2020-11-04 21:31:44 +0800

    Initial yesod backend with draft jwt

---
+++ project12/yesod-backend/src/Auth/JWT.hs
@@ -0,0 +1,49 @@
+{-
+Credit take reference from
+https://github.com/tzemanovic/haskell-yesod-realworld-example-app/blob/b0eb3bdc6b64358d59a4d5241283afeb8b530184/app/src/Auth/JWT.hs
+-}
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Auth.JWT
+  ( lookupToken
+  , jsonToToken
+  , tokenToJson
+  , UserId
+  )
+  where
+
+import           ClassyPrelude.Yesod
+import           Data.Char           (isSpace)
+import           Data.Map             as Map (fromList, (!?))
+import           Web.JWT              as JWT
+
+type UserId = String
+
+-- | Try to lookup token from the Authorization header
+lookupToken :: MonadHandler m => m (Maybe Text)
+lookupToken = do
+  mAuth <- lookupHeader "Authorization"
+  return $ extractToken . decodeUtf8 =<< mAuth
+
+-- | Create a token out of a given JSON 'Value'
+jsonToToken :: Text -> Value -> Text
+jsonToToken jwtSecret userId =
+  encodeSigned (JWT.hmacSecret jwtSecret) mempty
+    mempty {unregisteredClaims = ClaimsMap $ Map.fromList [(jwtKey, userId)]}
+
+-- | Extract a JSON 'Value' out of a token
+tokenToJson :: Text -> Text -> Maybe Value
+tokenToJson jwtSecret token = do
+    jwt <- JWT.decodeAndVerifySignature (JWT.hmacSecret jwtSecret) token
+    unClaimsMap (JWT.unregisteredClaims (JWT.claims jwt)) !? jwtKey
+
+jwtKey :: Text
+jwtKey = "jwt"
+
+extractToken :: Text -> Maybe Text
+extractToken auth
+  | toLower x == "token" = Just $ dropWhile isSpace y
+  | otherwise            = Nothing
+  where (x, y) = break isSpace auth
---
+++ project12/yesod-backend/src/Handler/User.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Handler.User where
+
+import Import
+import Yesod.Core.Json as YJ
+import qualified Auth.JWT as JWT
+
+postUserLoginR :: Handler YJ.Value
+postUserLoginR = do
+        jsonBody <- requireCheckJsonBody :: Handler Value
+        case (checkUserNamePassword jsonBody) of
+                Just v -> do
+                        -- TESTING START
+                        liftIO $ print.show $ v
+                        access_token <- fakeGen $ (fst v) ++ "_in_jwt"
+                        fakeTest access_token
+                        -- TESTING END
+                        returnJson (object ["access_token" .= (access_token::Text)])
+                _      -> sendResponseStatus unauthorized401 ()
+        
+checkUserNamePassword :: Value -> Maybe (String, String)
+checkUserNamePassword (Object jb) = do
+        (String username) <- lookup "username" jb 
+        (String password) <- lookup "password" jb
+        localCheck username password
+        where localCheck "tester" "password" = Just ("tester","password")
+              localCheck _        _          = Nothing
+checkUserNamePassword _         = Nothing
+
+ -- TESTING START
+fakeGen :: JWT.UserId -> HandlerFor App Text
+fakeGen userId = userIdToToken userId
+
+fakeTest :: Text -> Handler ()
+fakeTest token = do
+        $(logInfo) token
+        userId <- tokenToUserId token
+        case userId of
+                Just v -> liftIO $ print v
+                _      -> return ()
+-- TESTING END
