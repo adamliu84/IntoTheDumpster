5a48eaa2ec186796ea9234349f209b7e789294eb
adamliu84 <adam.liu84@gmail.com>
2020-11-29 00:55:49 +0800

    Temp code to retrieve the userid from token

--- project12/yesod-backend/src/Handler/Task.hs
+++ project12/yesod-backend/src/Handler/Task.hs
@@ -12,7 +12,10 @@ import Data.Text
 
 getTaskR :: Int -> Handler YJ.Value
 getTaskR taskId = do
+        userId <- maybeAuthId
         let title = Data.Text.append "Sample title #" (Data.Text.pack.show $ taskId)
+                    `Data.Text.append` " - By: "
+                    `Data.Text.append` (Data.Text.pack $ fromMaybe "Invalid_user" userId)
         return $ object [("task",object [
                                 ("title",String title),
                                 ("description",String "sample desc")
