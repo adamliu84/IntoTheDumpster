7502bfebd4b44cfeb36d276eeadc0445da1a0e46
adamliu84 <adam.liu84@gmail.com>
2021-06-06 18:22:39 +0800

    Trying out quickencrypt

--- project15/src/App.js
+++ project15/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { Base64 } from 'js-base64';
 import QRCode from 'qrcode.react';
+import QuickEncrypt from 'quick-encrypt';
 
 const GenBase64 = ({ sourceText }) => {
   const encodedText = Base64.encode(sourceText, true);
@@ -37,6 +38,31 @@ const GenQrCode = ({ sourceText }) => {
   );
 }
 
+const GenQuickEncrypt = ({ sourceText }) => {
+
+  let keys = QuickEncrypt.generate(2048) // Use either 2048 bits or 1024 bits.
+  const [encryptedText, setEncryptedText] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+
+  useEffect(() => {
+    if ("" !== sourceText) {
+      const encrypted = QuickEncrypt.encrypt(sourceText, keys.public)
+      setEncryptedText(encrypted);
+      setDecryptedText(QuickEncrypt.decrypt(encrypted, keys.private));
+    }
+  }, [sourceText])
+
+  return (
+    <>
+      <h3>QuickEncrypt</h3>
+      Public: <textarea id="qe_public" name="qe_public" rows="4" cols="50" defaultValue={keys.public} /><br />
+      Private: <textarea id="qe_private" name="qe_private" rows="4" cols="50" defaultValue={keys.private} />
+      <div>Encrypted Text: {encryptedText}</div>
+      <div>Decrypted Text: {decryptedText}</div>
+    </>
+  );
+}
+
 function App() {
 
   const [sourceText, setSourceText] = useState("");
@@ -52,6 +78,8 @@ function App() {
       <GenBase64 sourceText={sourceText} />
       <hr />
       <GenQrCode sourceText={sourceText} />
+      <hr />
+      <GenQuickEncrypt sourceText={sourceText} />
       <hr />
     </div>
   );
