fbe403643be12366ad70dee9ce89d84e7978ed38
adamliu84 <adam.liu84@gmail.com>
2025-02-26 22:48:37 +0800

    Prisma operation on READ, CREATE, UPDATE, DELETE

--- project24/server/index.js
+++ project24/server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const axios = require('axios');
 const bodyParser = require('body-parser')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+require('dotenv').config();
+const PRISMA_FLAG = process.env.PRISMA_FLAG === "TRUE"
 
 const app = express();
 app.use(bodyParser.json());
@@ -30,6 +34,9 @@ app.get("/api/users", (req, res) => {
 
 app.get("/api/posts/", async (req, res) => {
     try {
+        if (PRISMA_FLAG) {
+            prisma.post.count().then(nCount => { console.log(nCount) })
+        }
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
         return res.json(response.data)
     } catch (error) {
@@ -60,6 +67,16 @@ app.post("/api/post", async (req, res) => {
             userId: Math.floor(Math.random() * 100)
         });
         console.log("Creating post:" + JSON.stringify(response.data))
+        if (PRISMA_FLAG) {
+            const newPost = await prisma.post.create({
+                data: {
+                    title: newTitle,
+                    body: newBody,
+                    userId: Math.floor(Math.random() * 100)
+                }
+            })
+            console.log(newPost);
+        }
         return res.json(response.data)
     } catch (error) {
         console.error(error);
@@ -79,6 +96,27 @@ app.put("/api/post/:id", async (req, res) => {
             userId: Math.floor(Math.random() * 100)
         });
         console.log("Updating post:" + JSON.stringify(response.data))
+        if (PRISMA_FLAG) {
+            const oldPost = await prisma.post.findFirst({
+                orderBy: {
+                    id: 'asc',
+                },
+                take: 1
+            });
+            if (!oldPost) {
+                console.log("No existing post to mock update")
+            } else {
+                const updatePost = await prisma.post.update({
+                    where: { id: oldPost.id },
+                    data: {
+                        title: newTitle,
+                        body: newBody,
+                    }
+                }
+                )
+                console.log(updatePost);
+            }
+        }
         return res.json(response.data)
     } catch (error) {
         console.error(error);
@@ -91,6 +129,23 @@ app.delete("/api/post/:id", async (req, res) => {
         const id = req.params.id;
         const response = await axios.delete('https://jsonplaceholder.typicode.com/posts/' + id);
         console.log("Deleting post:" + JSON.stringify(response.data))
+        if (PRISMA_FLAG) {
+            const oldPost = await prisma.post.findFirst({
+                orderBy: {
+                    id: 'asc',
+                },
+                take: 1
+            });
+            if (!oldPost) {
+                console.log("No existing post to mock delete")
+            } else {
+                const deletePost = await prisma.post.delete({
+                    where: { id: oldPost.id },
+                }
+                )
+                console.log(deletePost);
+            }
+        }
         return res.json(response.data)
     } catch (error) {
         console.error(error);
